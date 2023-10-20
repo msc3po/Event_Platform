cd24109e8b1c23059febd684eb9743a790288e86
Manel Soler <manelsoler@gmail.com>
2023-10-20 13:18:19 +0200

    setting suser model

--- EventPlatform/settings.py
+++ EventPlatform/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+
 ]
 
 MIDDLEWARE = [
@@ -86,7 +87,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'EventPlatform.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
