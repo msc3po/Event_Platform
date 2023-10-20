c28b4141109981b7a59c865b8d75825a9289903f
Manel Soler <manelsoler@gmail.com>
2023-10-20 13:01:29 +0200

    user basic model is  created and migrated

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User (AbstractBaseUser
+            ):
+    email = models.EmailField(unique=True)
+    is_active= models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'
+
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+
